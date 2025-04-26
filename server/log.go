864cb39fcfe94fa3f77a9874b8715c4f9e38ad7a
sweeywu <3127484862@qq.com>
2025-04-26 12:42:05 +0800

    rpcserver

---
+++ server/log.go
@@ -0,0 +1,86 @@
+package server
+
+import (
+	"sync/atomic"
+)
+
+type Logger interface {
+	//调试
+	Debugf(format string, args ...interface{})
+	//追踪
+	Tracef(format string, args ...interface{})
+	//正常信息
+	Noticef(format string, args ...interface{})
+	//警告
+	Warnf(format string, args ...interface{})
+	//错误
+	Errorf(format string, args ...interface{})
+	//致命错误
+	Fatalf(format string, args ...interface{})
+}
+
+func (s *RPCServer) executeLogCall(f func(logger Logger, format string, args ...interface{}), format string, args ...interface{}) {
+	s.logging.RLock()
+	defer s.logging.RUnlock()
+	if s.logging.logger == nil {
+		return
+	}
+	f(s.logging.logger, format, args)
+}
+
+// 实现关于RPCServer的Logger接口
+// 调试
+func (s *RPCServer) Debugf(format string, args ...interface{}) {
+	if atomic.LoadInt32(&s.logging.debug) == 0 {
+		return
+	}
+	//为啥这个不能直接写成s.logging.Logger.Debugf(format,args)
+	s.executeLogCall(func(logger Logger, format string, args ...interface{}) {
+		logger.Debugf(format, args...)
+	}, format, args...)
+}
+
+// 追踪
+func (s *RPCServer) Tracef(format string, args ...interface{}) {
+	if atomic.LoadInt32(&s.logging.trace) == 0 {
+		return
+	}
+	s.executeLogCall(func(logger Logger, format string, args ...interface{}) {
+		logger.Tracef(format, args...)
+	}, format, args...)
+}
+
+// 正常信息
+func (s *RPCServer) Noticef(format string, args ...interface{}) {
+	s.executeLogCall(func(logger Logger, format string, args ...interface{}) {
+		logger.Noticef(format, args...)
+	}, format, args...)
+}
+
+// 警告
+func (s *RPCServer) Warnf(format string, args ...interface{}) {
+	s.executeLogCall(func(logger Logger, format string, args ...interface{}) {
+		logger.Warnf(format, args...)
+	}, format, args...)
+}
+
+// 错误
+func (s *RPCServer) Errorf(format string, args ...interface{}) {
+	s.executeLogCall(func(logger Logger, format string, args ...interface{}) {
+		logger.Errorf(format, args...)
+	}, format, args...)
+}
+
+// 致命错误
+func (s *RPCServer) Fatalf(format string, args ...interface{}) {
+	s.executeLogCall(func(logger Logger, format string, args ...interface{}) {
+		logger.Fatalf(format, args...)
+	}, format, args...)
+}
+
+// 现在写这个感觉没用上
+func (s *RPCServer) Logger() Logger {
+	s.logging.RLock()
+	defer s.logging.RUnlock()
+	return s.logging.logger
+}
--- server/rpcserver.go
+++ server/rpcserver.go
@@ -3,10 +3,19 @@ package server
 import (
 	"Wx_MQ/kitex_gen/api/server_operations"
 
+	"sync"
+
 	"github.com/cloudwego/kitex/server"
 )
 
 type RPCServer struct {
+	logging struct {
+		logger      Logger
+		debug       int32 //是否开启调试日志
+		trace       int32 //是否开启trace日志
+		traceSysAcc int32 //trace system account是否开启系统用户的trace日志
+		sync.RWMutex
+	}
 	server Server
 }
 
